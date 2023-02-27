5788a55170244f2db327db2eff55afcc1d22054c
Dino Ribeiro <DRibeiro1124@gmail.com>
2023-02-27 17:32:53 -0500

    created mumbling controller

--- Controllers/ExplosionController.cs
+++ Controllers/ExplosionController.cs
@@ -1,4 +1,40 @@
 using System;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace ExplosionAPI.Controllers
+{
+    [ApiController]
+    [Route("/digits")]
+    public class DigitsExplosion
+    {
+        [HttpGet("(s)")]
+        public string Explode(string s)
+        {
+            string solution = "";
+
+            foreach (char c in s)
+            {
+                int number = int.Parse(c.ToString());
+
+                for (int i = number; i > 0; i--)
+
+                {
+                    solution += number;
+                }
+            }
+            return solution;
+
+        }
+    }
+}
+
+
+/*
+
+
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -11,5 +47,23 @@ namespace ExplosionAPI.Controllers
     [ApiController]
     public class ExplosionController : ControllerBase
     {
+        [HttpGet("explosion")]
+        public string Explode(string str)
+        {
+            string result = "";
+
+            foreach (char c in str)
+            {
+                int number = int.Parse(c.ToString());
+
+                for (int i = number; i > 0; i--)
+                {
+                    result += number;
+                }
+
+                return result;
+            }
+        }
     }
 }
+*/
